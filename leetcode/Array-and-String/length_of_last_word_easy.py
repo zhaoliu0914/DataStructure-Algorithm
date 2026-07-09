6558563336a44797e67a8e64d6c78cbdbb0b3b71
zhaoliu <master.liuzhao@gmail.com>
2026-07-09 19:55:57 -0400

    complete for Length of Last Word

--- leetcode/Array-and-String/length_of_last_word_easy.py
+++ leetcode/Array-and-String/length_of_last_word_easy.py
@@ -27,8 +27,34 @@ There will be at least one word in s.
 
 
 def lengthOfLastWord(s: str) -> int:
-    pass
+    size = len(s)
+    last_word = []
+    is_last_word = False
+    for i in range(size - 1, -1, -1):
+        current_char = s[i]
+        #print(f"i = {i}, is_last_word = {is_last_word}, current_char = {current_char}")
+        if current_char == " ":
+            if is_last_word:
+                break
+        else:
+            is_last_word = True
+            last_word.append(current_char)
+    return len(last_word)
 
 
 if __name__ == '__main__':
-    pass
+    s = "Hello World"
+    last_word_length = lengthOfLastWord(s)
+    print(f"s = {s}, last_word_length = {last_word_length}")
+
+    s = "   fly me   to   the moon  "
+    last_word_length = lengthOfLastWord(s)
+    print(f"s = {s}, last_word_length = {last_word_length}")
+
+    s = "luffy is still joyboy"
+    last_word_length = lengthOfLastWord(s)
+    print(f"s = {s}, last_word_length = {last_word_length}")
+
+    s = "job"
+    last_word_length = lengthOfLastWord(s)
+    print(f"s = {s}, last_word_length = {last_word_length}")
