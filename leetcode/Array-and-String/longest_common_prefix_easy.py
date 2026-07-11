e6df7cb57df144c1d728543a540fe8200cfe7d07
zhaoliu <master.liuzhao@gmail.com>
2026-07-10 23:01:45 -0400

    complete Longest Common Prefix

--- leetcode/Array-and-String/longest_common_prefix_easy.py
+++ leetcode/Array-and-String/longest_common_prefix_easy.py
@@ -21,8 +21,38 @@ strs[i] consists of only lowercase English letters if it is non-empty.
 
 
 def longestCommonPrefix(strs: list[str]) -> str:
-    pass
+    size = len(strs)
+    smallest_len = 100000
+    for i in range(size):
+        length = len(strs[i])
+        if length < smallest_len:
+            smallest_len = length
+    index = 0
+    is_continue = True
+    prefix = ""
+    while index < smallest_len:
+        current_char = strs[0][index]
+        for i in range(1, size):
+            if current_char != strs[i][index]:
+                is_continue = False
+                break
+
+        index += 1
+        if is_continue == False:
+            break
+        prefix = f"{prefix}{current_char}"
+    return prefix
 
 
 if __name__ == '__main__':
-    pass
+    strs = ["flower", "flow", "flight"]
+    prefix = longestCommonPrefix(strs)
+    print(f"strs = {strs}, prefix = {prefix}")
+
+    strs = ["dog", "racecar", "car"]
+    prefix = longestCommonPrefix(strs)
+    print(f"strs = {strs}, prefix = {prefix}")
+
+    strs = [""]
+    prefix = longestCommonPrefix(strs)
+    print(f"strs = {strs}, prefix = {prefix}")
