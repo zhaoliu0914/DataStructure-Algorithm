4d0c39f1d2fd2bd98fa21045f7f7fc8b00a411ef
zhaoliu <master.liuzhao@gmail.com>
2026-08-16 23:40:03 -0400

    update for Spiral Matrix

--- leetcode/matrix/spiral_matrix_medium.py
+++ leetcode/matrix/spiral_matrix_medium.py
@@ -22,8 +22,7 @@ n == matrix[i].length
 def spiralOrder(matrix: list[list[int]]) -> list[int]:
     m = len(matrix)
     n = len(matrix[0])
-    print(f"m = {m}")
-    print(f"n = {n}")
+    print(f"m = {m}, n = {n}")
     for i in range(m):
         pass
     for i in range(n):
