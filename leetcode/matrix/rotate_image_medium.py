ee24260e0a9f32e2f0865187073e78b18c76d4a8
zhaoliu <master.liuzhao@gmail.com>
2026-09-09 22:07:47 -0400

    update

--- leetcode/matrix/rotate_image_medium.py
+++ leetcode/matrix/rotate_image_medium.py
@@ -45,6 +45,7 @@ def rotate(matrix: list[list[int]]) -> None:
     # [2,2] -> [2,0]
 
     """
+    First solution: Transpose, Reverse
     1 2 3        1 4 7        7 4 1
     4 5 6   ->   2 5 8   ->   8 5 2
     7 8 9        3 6 9        9 6 3
@@ -58,6 +59,11 @@ def rotate(matrix: list[list[int]]) -> None:
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
+
+
+    """
+    Second solution: four-way cycle swap
+    """
 
 
 if __name__ == "__main__":
